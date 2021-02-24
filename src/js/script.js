179918ae7b4b6d4dbb4b75ab56855de569d853a1
Murat Cengiz <mcengiz2402@gmail.com>
2021-02-24 14:43:26 +0100

    Remove unnecessary code

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,3 @@
-if (typeof apiBaseUrl === 'undefined') {
-  // Default API Base URL if not defined
-  apiBaseUrl = 'https://grafana-chart-dev.apps.c1.ocp.dev.sgcip.com';
-}
-
 let defaultItems;
 const periods = [
   'w', // week
@@ -131,24 +126,6 @@ async function createMultipleSelectionList(kind) {
   return multipleSelection
 }
 
-async function loadComponents() {
-  const components = await callApi('GET', `${apiBaseUrl}/components`)
-  components.sort(sortByName) // Sort alphabetically
-
-  return components
-}
-
-async function loadCompanies() {
-  const companies = await callApi('GET', `${apiBaseUrl}/companies`)
-  companies.sort(sortByName) // Sort alphabetically
-
-  return companies
-}
-
-async function loadStack(name) {
-  return await callApi('GET', `${apiBaseUrl}/stacks/${name}/details`)
-}
-
 async function componentsCallback() {
   const query = new URLSearchParams(window.location.search)
   const queryStack = query.get('stack');
@@ -179,59 +156,6 @@ async function companiesCallback() {
   }
 
   return await loadCompanies();
-}
-
-async function callApi(method, url, data) {
-  // Default options are marked with *
-  const config = {
-    method: method, // *GET, POST, PUT, DELETE, etc.
-    /*
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    */
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      //'Access-Control-Allow-Origin': '*',
-      //'Connection': 'keep-alive',
-      //'Host': 'k8s.devstats.cncf.io',
-      //'Acept-Encoding': 'gzip, deflate, br',
-      //'USer-Agent': 'PostmanRuntime/7.26.8',
-    },
-    /*
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'strict-origin-when-cross-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    */
-  };
-
-  if (data) config.body = JSON.stringify(data);
-
-  const response = await fetch(url, config);
-  if (!response.ok) {
-    throw new Error("HTTP status " + response.status);
-  }
-
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-function sortByName(a, b) {
-  const aName = (a.name || a).toLowerCase();
-  const bName = (b.name || b).toLowerCase();
-
-  if (aName === bName) return 0;
-
-  const aLatin = isLatinLetter(aName[0]);
-  const bLatin = isLatinLetter(bName[0]);
-
-  if (aLatin && !bLatin) return -1;
-  else if (!aLatin && bLatin) return 1;
-
-  return aName < bName ? -1 : 1;
-}
-
-function isLatinLetter(letter) {
-  return letter.toUpperCase() !== letter.toLowerCase()
 }
 
 run();
